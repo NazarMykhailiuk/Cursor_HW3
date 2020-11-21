8a6aedb9d3dc197149872a5f96ee1db061b4cc5e
NazarMykhailiuk <Nazar.Mykhailiuk@gmail.com>
2020-11-21 21:07:46 +0200

    Update Car.java

--- src/com/company/Car.java
+++ src/com/company/Car.java
@@ -2,48 +2,60 @@ package com.company;
 
 public class Car {
     private String Model;
-    private boolean engineRunning;
-    private float speed;
-    private int gear;
+    public boolean engineRunning;
+    public boolean going;
+    public float speed;
+    public float increaseSpeed;
 
-    public Car(String modelGiven, boolean engineRunningGiven, int gearGiven, float speedGiven) {
-        Model = modelGiven;
-        engineRunning = engineRunningGiven;
-        gear = gearGiven;
-        speed = speedGiven;
-        System.out.print(modelGiven);
+    public float getSpeed() {
+        return this.speed;
+    }
 
+    public void setSpeed(float speed) {
+        this.speed = speed;
+    }
+
+    public void startEngine() {
         if (engineRunning) {
-            engineRunning = true;
-            System.out.print(" engine running");
-            if (speed > 200) {
-                speed = 200;
-            }
-            if (gear > 4) {
-                gear = 4;
-            }
-            if (speed < 40) {
-                gear = 1;
-            } else if (speed < 90) {
-                gear = 2;
-            } else if (speed < 150) {
-                gear = 3;
-            } else {
-                gear = 4;
-            }
-            if (speed > 0 & gear > 0) {
-                System.out.println(". You drive with speed " + speed + " km/h. in " + gear + " gear.");
-            } else {
-                System.out.println(". You not drive.");
-            }
+            System.out.println("Engine has been started");
         } else {
-            System.out.println(" engine not running. You not drive.");
+            System.out.println("Engine is already run");
         }
-
     }
 
-    public static void main(String[] args) {
-        Car car1 = new Car("Mazda", true, 1, 20.5f);
+    public void stopEngine() {
+        if (engineRunning) {
+            System.out.println("Engine has been stopped");
+        } else {
+            System.out.println("Engine is already stopped");
+        }
     }
 
+    public void accelerating() {
+        System.out.println("Accelerating");
+        for (int i = 0; i < 5; i++) {
+            increaseSpeed += 5;
+            System.out.println(increaseSpeed + speed);
+        }
+        speed = getSpeed();
+    }
+
+    public void breaking() {
+        System.out.println("Breaking");
+        for (int i = 0; i < 5; i++) {
+            increaseSpeed -= 5;
+            System.out.println(increaseSpeed + speed);
+        }
+        speed = getSpeed();
+    }
+
+    public void stopCar() {
+        if (going) {
+            going = false;
+            speed = 0.0f;
+            System.out.println("Car is stopped");
+        } else {
+            System.out.println("Car is already stopped");
+        }
+    }
 }
